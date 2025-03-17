cff82d43dca3badf5e53b3701c4616b728dd3a8a
Kory Fargo <kfargo@precoa.com>
2025-03-17 11:59:24 -0500

    add basename on router

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import App from "./App.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/kory-fetch-takehome">
         <App />
       </BrowserRouter>
     </Provider>
